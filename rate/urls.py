1ac8b8d8b68747c685c80375ca46ffc4ed896cb0
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-09-20 16:11:30 +0300

    rating project url

--- rate/urls.py
+++ rate/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path("post/project", views.post_project, name="post_project"),
     path('search/', views.project_search, name = "project_search"),
     path('profile/',views.profile, name = "profile"),
+    path('project/rate/<int:id>', views.rate_project, name ="rate_project"),
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
